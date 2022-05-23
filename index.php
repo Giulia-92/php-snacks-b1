b334edc18eff262435ebfc46e77d5208aa5ba8d1
Giulia-92 <cionco.giulia@gmail.com>
2022-05-23 16:02:29 +0200

    php-snacks-b1

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+
+$parola = $_GET['badword']
+
+$lista = [
+    'pane',
+    'zucchero',
+    'acqua'
+];
+
+$lista_migliorata = [
+    'da_prendere' => [
+        'pane'
+    ],
+    'gia_presi' => [
+        'acqua'
+    ]
+];
+$lista_migliorata['da_prendere'];
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack</title>
+</head>
+<body>
+    
+</body>
+</html>
